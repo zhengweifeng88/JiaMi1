ce974698b7d72ae8682585ba325721587f33bc35
zhengweifneg88 <1396168521@qq.com>
2017-12-12 15:31:26 +0800

    Initial commit

---
+++ src/main/java/test/bwei/com/duoxianchengdown/DownloadProgressListener.java
@@ -0,0 +1,10 @@
+package test.bwei.com.duoxianchengdown;
+
+/**
+ * Created by Administrator on 2017/11/29.
+ */
+
+public interface DownloadProgressListener {
+    public void onDownloadSize(int size);
+
+}
